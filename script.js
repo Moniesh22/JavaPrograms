bf9002eaac88d04bd114d43050ccdf28e16979be
MONISH S <159697718+Moniesh22@users.noreply.github.com>
2025-01-07 23:57:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 document.getElementById('show-message-btn').addEventListener('click', function() {
     if (confirm('Are you sure you want to show the message?')) {
         document.getElementById('message-container').style.display = 'block';
-        document.getElementById('message').innerText = 'Hᴇʏ ʜɪ 🖖
-Hᴀᴘᴘʏ ɴᴇᴡ ᴊᴏᴜʀɴᴇʏ ᴏꜰ Yᴏᴜʀ ʟɪꜰᴇ🚶‍♀️...
-Yᴏᴜ 👑 ᴅᴇꜱᴇʀᴠᴇ ᴀʟʟ Lᴏᴠᴇ ᴀɴᴅ Cᴀʀᴇ ɪɴ ᴛʜɪꜱ Aᴄᴄᴜʀꜱᴇᴅ Wᴏʀʟᴅ 🌍 .
-Rᴇᴍᴇᴍʙᴇʀ ᴍʏ ᴡᴏʀᴅꜱ 💭 ɪ ᴡɪʟʟ ɴᴇᴠᴇʀ ꜰᴏʀɢᴇᴛ ʏᴏᴜ ᴀɴʏᴡᴀʏ & ʏᴏᴜ ᴀʀᴇ ᴄʀᴜᴄɪᴀʟ ꜱᴏᴜʟ ꜰᴏʀ Mᴇ 😇 
-🤍 Tᴀᴋᴇ ᴄᴀʀᴇ ᴏꜰ ʏᴏᴜʀꜱᴇʟꜰ Fᴏʀᴇᴠᴇʀ .';
+        document.getElementById('message').innerText = 'Happy Birthday! Wishing you a day filled with love, laughter, and all your favorite things!';
     }
+});
+
+document.getElementById('close-btn').addEventListener('click', function() {
+    document.getElementById('message-container').style.display = 'none';
 });

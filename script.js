ac708e9a5db0bdbadb29b5bf72419cf69bc38378
MONISH S <159697718+Moniesh22@users.noreply.github.com>
2025-01-08 09:20:47 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('show-message-btn').addEventListener('click', function() {
-    if (confirm('Are you sure you want to show the message?')) {
-        document.getElementById('show-message-btn').style.display = 'none';
-        document.getElementById('page-flip-container').style.display = 'flex';
-        setTimeout(function() {
-            document.getElementById('page-flip-container').style.display = 'none';
-            document.getElementById('sparkles-container').style.display = 'flex';
-            document.getElementById('papers-container').style.display = 'flex';
